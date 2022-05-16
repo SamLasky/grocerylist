baaac4fa7ba54fc52c83ca4489b8bb8abe10d7ab
SamLasky <46426486+SamLasky@users.noreply.github.com>
2022-05-16 16:57:23 -0400

    Added searchbar

--- src/components/Searchbar.js
+++ src/components/Searchbar.js
@@ -1,1 +1,16 @@
 import React from 'react';
+
+const SearchBox = ({ searchfield, searchChange }) => {
+  return (
+    <div className='pa2'>
+      <input
+        className='pa3 ba b--green bg-lightest-blue'
+        type='search'
+        placeholder='grocery item'
+        onChange={searchChange}
+      />
+    </div>
+  );
+}
+
+export default Searchbar;
--- src/containers/App.js
+++ src/containers/App.js
@@ -2,18 +2,34 @@ import './App.css';
 import Searchbar from '../components/Searchbar';
 import Checkbox from '../components/Checkbox';
 import ListItem from '../components/ListItem';
+import Data from "../mock-data.json"
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h1>Grocery Checklist</h1>
-        <p>
-          This is where a searchbar will gornt!
-        </p>
       </header>
+
     </div>
+
+export default function App (){
+  return {
+    <div>
+      <input placeholder="Enter Post Title"/>
+    </div>
+ }
+}
   );
+}
+
+{
+  Data.map((post) => (
+    <div key={post.id}>
+      <p>{post.title}</p>
+      <p>{post.author}</p>
+    </div>
+  ));
 }
 
 export default App;
